f285371c0035af334ca10b3813aaf4de2fe3fa6b
zainbsuliman <zat301099@gmail.com>
2024-12-16 09:14:03 +0300

    Initial Commit

---
+++ ProjectDeyslexia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectDeyslexia
+//
+//  Created by Zainab Alatwi  on 15/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectDeyslexia/ProjectDeyslexiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectDeyslexiaApp.swift
+//  ProjectDeyslexia
+//
+//  Created by Zainab Alatwi  on 15/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ProjectDeyslexiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
